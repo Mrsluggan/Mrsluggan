3729a662a50e8cffeae531d40970b5c1948d2e62
mrsluggan <ericflyger@gmail.com>
2024-04-04 18:22:18 +0200

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,13 @@ export default function Home() {
                 backgroundColor="black"
             />
             <div class="h-screen flex items-center justify-center">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/Urn3YLPwxoE?si=HmSty6eqqNX4NnbV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>            </div>
+
+                <video width="320" height="240" controls>
+                    <source src="glock.mov" type="video/mp4"></source>
+
+                </video>
+
+            </div>
 
 
         </>
