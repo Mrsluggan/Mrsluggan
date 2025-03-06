10de5f29c72117e2f01f2ad5bd38887738905f1f
mrsluggan <ericflyger@gmail.com>
2025-03-06 19:46:33 +0100

    navbar changes

--- sluggan/src/pages/Home.tsx
+++ sluggan/src/pages/Home.tsx
@@ -1,11 +1,21 @@
+import '../assets/Home.css';  // Ladda in en extern CSS-fil
 
-export default function Home(){
-
-
+export default function Home() {
     return (
-        <>
-            <h1>Hello</h1>
+        <section className="home-container">
+            <div className="left-column">
+                <span className="sluggan-header">
+                    <span className="sluggan-icon">👋</span>
+                    <span className="sluggan-text">Hello, I'm Sluggan </span>
+                    <span className="sluggan-icon">🐌</span>
 
-        </>
+                </span>
+                <p className="description">
+                    I am an experienced software developer with expertise in both frontend and backend technologies.
+                    My focus is on creating scalable, efficient, and robust web applications.
+                </p>
+            </div>
+
+        </section>
     );
 }
