eb8c6c62b86bc6a9b70ababd4f340f420a44d63b
Mrsluggan <Mrsluggan@users.noreply.github.com>
2026-09-23 13:12:34 +0000

    deploy: 06bce2c6c20a8b08e1764374df5dde99e0590a83

---
+++ assets/en-DpvxPpEb.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,r as o,A as r}from"./App-Ba2V6KfU.js";e.hydrateRoot(document.getElementById("root"),t.jsx(o.StrictMode,{children:t.jsx(r,{locale:"en"})}));
--- assets/main-B1p47yCf.js
+++
@@ -1,1 +0,0 @@
-import{c as o,j as t,r as e,A as r}from"./App-C9fypJGr.js";o.hydrateRoot(document.getElementById("root"),t.jsx(e.StrictMode,{children:t.jsx(r,{locale:"sv"})}));
